7106df395a7402244c1aff87bc9d44985820b3c7
tenzinsean <tenzin.jeunesse@gmail.com>
2018-11-07 17:37:09 +0100

    Update again

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import TodoItme
 
 
 # Create your views here.
 class HomePageView(TemplateView):
+    all_todo_items = TodoItme.objects.all()
     template_name = "home.html"
